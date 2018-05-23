0853a2a88586214b8bf1fdb926611d65117dd31f
Attila_Kristof_Foldi <Attila_Kristof_Foldi@epam.com>
2018-05-23 09:45:36 +0200

    restructuring code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require( 'fs' );
-const mParser = require( './m-parser' );
+const mParser = require( './parser/m-parser' );
 
 console.time( 'read-file' );
-fs.readFile( './proba.md', 'utf-8', function ( err, data ) {
+fs.readFile( './test-data/proba.md', 'utf-8', function ( err, data ) {
   if ( err ) throw err;
   console.timeEnd( 'read-file' );
   console.time( 'process-file' );
   const result = mParser.parse( data );
   console.timeEnd( 'process-file' );
-  console.log( JSON.stringify( result, null, 2 ) );
+  //console.log( JSON.stringify( result, null, 2 ) );
 } )
