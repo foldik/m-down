a71e2a8df157d8ff1b5e31ed8f3fd41fc4344e6f
Attila_Kristof_Foldi <Attila_Kristof_Foldi@epam.com>
2018-05-24 17:09:17 +0200

    recursive file processing

---
+++ cli.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const fs = require( 'fs' );
+const mParser = require( './parser/m-parser' );
+const mRenderer = require( './renderer/html-renderer' );
+const argv = require( 'minimist' )( process.argv.slice( 2 ) );
+
+if ( !argv.inDir || !argv.outDir ) {
+  console.log( 'You must give the input directory and the output directory like:\r\n\r\n\tm-down --inDir=content --outDir=dist' );
+  process.exit( -1 );
+}
+
+console.log( `Input directory: ${argv.inDir}` );
+console.log( `Output directory: ${argv.outDir}` );
+
+if ( fs.existsSync( argv.outDir ) ) {
+  console.log( `Clean ${argv.outDir} directory` );
+  fs.rmdirSync( argv.outDir );
+}
+fs.mkdirSync( argv.outDir );
+
+function copyDirectory( dir ) {
+  const files = fs.readdirSync( dir );
+  files.forEach( function ( file ) {
+    const currentFile = dir + '/' + file;
+    const resultDirectory = argv.outDir + '/' + currentFile.replace( argv.inDir + '/', '' );
+    if ( fs.statSync( currentFile ).isDirectory() && !fs.existsSync( resultDirectory ) ) {
+      fs.mkdirSync( resultDirectory );
+      console.log( resultDirectory );
+      copyDirectory( currentFile );
+    }
+  } );
+};
+
+function toHtml( inputFile, outputFile ) {
+  console.time( `Reading ${inputFile}` );
+  fs.readFile( inputFile, 'utf-8', function ( err, data ) {
+    if ( err ) throw err;
+    console.timeEnd( `Reading ${inputFile}` );
+
+    console.time( `Parsing ${inputFile}` );
+    const parsed = mParser.parse( data );
+    console.timeEnd( `Parsing ${inputFile}` );
+
+    console.time( `Rendering ${inputFile}` );
+    const html = mRenderer.render( parsed );
+    console.timeEnd( `Rendering ${inputFile}` );
+
+    console.time( `Saving ${outputFile}` );
+    fs.readFile( './template.html', 'utf-8', function ( err, data ) {
+      if ( err ) throw err;
+      fs.writeFile( outputFile, data.replace( 'CONTENT', html ), 'utf-8', function ( err, data ) {
+        if ( err ) throw err;
+        console.timeEnd( `Saving ${outputFile}` );
+      } );
+    } );
+
+  } );
+}
+
+function processMarkdownFiles( dir ) {
+  const files = fs.readdirSync( dir );
+  files.forEach( function ( file ) {
+    const currentFile = dir + '/' + file;
+    if ( fs.statSync( currentFile ).isDirectory() ) {
+      processMarkdownFiles( currentFile );
+    } else if ( fs.statSync( currentFile ).isFile() ) {
+      const resultFile = argv.outDir + '/' + currentFile.replace( argv.inDir + '/', '' ).replace( '.md', '.html' );
+      toHtml( currentFile, resultFile );
+    }
+  } );
+};
+
+console.log( `Copy folder structure into ${argv.outDir} directory` );
+copyDirectory( argv.inDir );
+processMarkdownFiles( argv.inDir );
--- test-data/folder-1/file-1.md
+++ test-data/folder-1/file-1.md
@@ -10,7 +10,7 @@
 
 ###### Header 6
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in _voluptate velit esse_ cillum dolore *eu fugiat nulla* pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. [Kecske](Cica)
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in _voluptate velit esse_ cillum dolore *eu fugiat nulla* pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. [README.md](/README.html)
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, **sed do eiusmod tempor incididunt ut** labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
@@ -37,7 +37,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod _tempor 
 **Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed**
 
 - This is a simple list
-- This is a simple list [Link](link.md) *Hey*
+- This is a simple list [README.md](/README.html) *Hey*
 - This is a simple list **Good** to see **You**.
 - This is a simple list
 - This is a simple list
